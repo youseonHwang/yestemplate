49c9d4ed26d54285ecea8d9d3b23acd708717856
youseonHwang <dbtjs0221@gmail.com>
2021-04-15 10:45:14 +0900

    회원가입 수정

--- src/routes/api/user.ts
+++ src/routes/api/user.ts
@@ -13,14 +13,13 @@ const router: Router = Router();
 { /* user 회원가입 */ }
 router.post("/",
   [
-    check("")
-    check("email", "Please include a valid email").isEmail(),
-    check(
-      "password",
-      "Please enter a password with 6 or more characters"
-    ).isLength({ min: 6 })
+    check("name", "이름을 작성해주세요").isLength({ min: 1 }),
+    check("email", "유효한 이메일 주소를 작성해주세요").isEmail(),
+    check("password", "비밀번호는 최소 6글자 이상 입력해주세요").isLength({ min: 6 })
   ],
+
   async (req: Request, res: Response) => {
+    // check 결과 에러 여부 확인
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res
@@ -30,6 +29,8 @@ router.post("/",
 
     const { name, email, password } = req.body;
     try {
+
+      // 가져온 email을 가지고 User가 이미 존재하는지 확인
       let user: IUser = await User.findOne({ email });
 
       if (user) {
@@ -42,6 +43,7 @@ router.post("/",
         });
       }
 
+      // 기존 User가 없다면 가져온 password로 암호화
       const salt = await bcrypt.genSalt(10);
       const hashed = await bcrypt.hash(password, salt);
 
@@ -55,22 +57,10 @@ router.post("/",
 
       user = new User(userFields);
 
-      await user.save();
-
-      const payload: Payload = {
-        userId: user.id
-      };
-
-      // token 생성
-      jwt.sign(
-        payload,
-        config.get("jwtSecret"),
-        { expiresIn: config.get("jwtExpiration") },
-        (err, token) => {
-          if (err) throw err;
-          res.json({ token });
-        }
-      );
+      // User 저장
+      user = await user.save();
+      console.log(user);
+      res.json(user);
     } catch (err) {
       console.error(err.message);
       res.status(HttpStatusCodes.INTERNAL_SERVER_ERROR).send("Server Error");
