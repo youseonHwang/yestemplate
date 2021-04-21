1bc8865359aaa6becdd140f099b26308768f2354
youseonHwang <dbtjs0221@gmail.com>
2021-04-21 14:37:18 +0900

    [슨슨] rightPreview 컴포넌트 생성중

--- client/src/components/pages/myPage/MyPage.tsx
+++ client/src/components/pages/myPage/MyPage.tsx
@@ -46,7 +46,8 @@ const MyPageComponent: React.FC<Props> = ({
               return (
                 <>
                   <TemplateCard key={index} template={template} />
-                </>)
+                </>
+              )
             })}
           </Grid>
         </Grid>
